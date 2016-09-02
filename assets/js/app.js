cdf7381ef90381150dfd3e0ce68ea526720f6e2b
An Huynh <An Huynh>
2016-09-01 20:14:24 -0500

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 var triviaQuestionArray = [{
 	question: 'Who is Agent Cooper?',
-	possibleAnswers: ['yes', 'no', 'maybe', 'never'],
-	correctAnswer: 'yes',
+	possibleAnswers: ['FBI Agent sent to investigate Laura Palmer\'s death', 'Laura Palmer\'s father', 'No one', 'The log man'],
+	correctAnswer: 'FBI Agent sent to investigate Laura Palmer\'s death',
 }, {
 	question: 'What does the crazy log lady hold and speak to?',
-	possibleAnswers: ['yes', 'no', 'maybe', 'her friendly log'],
-	correctAnswer: 'maybe',
+	possibleAnswers: ['Mr.Pibbles her kitty cat', 'She holds and speaks to herself', 'Nothing', 'Her friendly log'],
+	correctAnswer: 'Her friendly log',
 }, {
 	question: 'Who was Laura Palmer\'s best friend?',
 	possibleAnswers: ['Audrey','Billy','Shelly','Donna'],
@@ -15,45 +15,67 @@ var triviaQuestionArray = [{
 var questionNumber = 1;
 var correct = 0;
 var incorrect = 0;
+var countDown = 30;
 
 function addClickListeners() {
 	$('#start').click(triviaGame);
-	$('#next').hide();
-
 	$('#next').click(triviaGame);
 	$('#restart').click(triviaGame);
 }
 
 function triviaGame() {
+	console.log(questionNumber);
 	$('#start').hide();
 	$('#restart').hide();
 	$('#next').show();
+
+	$('#questionNumber').empty();
 	$('#triviaQuestion').empty();
 	$('#answerOption').empty();
-	$('#triviaQuestion').append(triviaQuestionArray[questionNumber - 1].question);
-		console.log(triviaQuestionArray[(questionNumber) - 1].question);
-		console.log(triviaQuestionArray[questionNumber - 1].possibleAnswers);
-	
 
-	if (questionNumber > 4) {
+	if (questionNumber > 3) {
 		reset();
 	}
+
+	$('#questionNumber').append("Question Number: " + questionNumber);
+		console.log(questionNumber);
+	$('#triviaQuestion').append(triviaQuestionArray[questionNumber - 1].question);
+		console.log(triviaQuestionArray[(questionNumber) - 1].question);
+	$('#answerOption').append(triviaQuestionArray[questionNumber - 1].possibleAnswers);
+		console.log(triviaQuestionArray[questionNumber - 1].possibleAnswers);
+	
 	questionNumber++;
 	console.log(questionNumber);
 
+		timer();
 }
 
 function reset () {
 		console.log(questionNumber);
 		$('#triviaQuestion').empty();
 		$('#answerOption').empty();
+
+		$('#questionNumber').hide();
 		$('#restart').show();
 		$('#next').hide();
-		questionNumber = 1;
+
+		$('triviaQuestion').append("Game Over!")
+}
+
+function timer(){
+	setTimeout(decrement, 3000);
+	console.log("3 seconds");
+}
+
+function decrement(){
+	countDown--;
+	$('#timerCountdown').html("Time Left: " + countDown);
 }
 
 $(document).ready(function(){
+
 	addClickListeners();
+
 
 });
 
